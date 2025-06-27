0df22a080d7abb71e1c9c52b84c79b521f019e88
Thwin Khant Oo <thwinkhantoo@uit.edu.mm>
2025-06-28 02:00:35 +0630

    edit clear_queue.php

--- public/clear_queue.php
+++ public/clear_queue.php
@@ -1,5 +1,5 @@
 <?php
-$queue_file = __DIR__ . '/public/admin_queue.txt';
+$queue_file = __DIR__ . '/admin_queue.txt';
 file_put_contents($queue_file, ''); // Clear file content
 echo "Queue cleared.";
 ?>
